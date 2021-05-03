075b26f61cddc10e2ba4f1bf23768ecfa9d4a0e5
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2021-05-03 17:50:34 +0300

    raise and handle event

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        value: this.props.value
+        value: this.props.counter.value
     };
 
     handleIncrement = () => {
@@ -13,7 +13,12 @@ class Counter extends Component {
         return (
         <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        <button 
+            onClick={this.handleIncrement} 
+            className="btn btn-secondary btn-sm">Increment</button>
+        <button 
+            onClick={() => this.props.onDelete(this.props.counter.id)} 
+            className="btn btn-danger btn-sm m-2">Delete</button>
         </div>);
     }
 
